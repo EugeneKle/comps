b92bf13d4d0304dda12353c93f94fb12a3f8829e
Eugene <klev.evzen@gmail.com>
2023-01-09 14:41:08 +0100

    new components Button and Accordion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import ButtonPage from "./pages/ButtonPage";
+import Accordion from "./cmoponents/Accordion";
+
 function App() {
-  return <div>App</div>;
+  const items = [
+    {
+      id: "sasd",
+      label: "Can I use React on a project",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+    },
+    {
+      id: "safdv",
+      label: "Can I use JavaScript on a project",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+    },
+    {
+      id: "amaks",
+      label: "Can I Use React on a project",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+    },
+  ];
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,39 @@
+import { GoBell, GoCloudUpload, GoDatabase } from "react-icons/go";
+import Button from "../cmoponents/Button";
+
+function ButtonPage() {
+  return (
+    <div>
+      <div>
+        <Button secondary outline rounded className={"mb-5"}>
+          <GoBell />
+          Click me!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline rounded>
+          <GoCloudUpload />
+          Buy noew!
+        </Button>
+      </div>
+      <div>
+        <Button warning rounded>
+          <GoDatabase />
+          Subscrube
+        </Button>
+      </div>
+      <div>
+        <Button primary outline>
+          Follow me
+        </Button>
+      </div>
+      <div>
+        <Button success rounded>
+          Success button
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
