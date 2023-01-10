3094a233460f8f21be0982734b4e8a5a46db7dec
Eugene <klev.evzen@gmail.com>
2023-01-10 16:56:49 +0100

    added navigation context, Link comp, Route comp

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Dropdown from "../components/Dropdown";
+
+function DropdownPage() {
+  const [selection, setSelection] = useState({});
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Yellow", value: "yellow" },
+    { label: "Green", value: "green" },
+    { label: "Orange", value: "orange" },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
+}
+
+export default DropdownPage;
